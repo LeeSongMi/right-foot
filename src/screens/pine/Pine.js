fb103584411fe17069945adaaa3030977b47004a
LeeSongMi <lssong9631@gmail.com>
2021-02-25 21:14:46 +0900

    플레이 페이지 추가

--- src/screens/pine/Pine.js
+++ src/screens/pine/Pine.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+
 const Pine = () => {
   return (
     <Body className="Body d-flex ">
--- src/screens/pine/Play.js
+++ src/screens/pine/Play.js
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
 const Play = () => {
-  return <div>딱지 치는 화면</div>;
-};
+  return (
+    <>
+      <h3 className="w-100 text-center">딱지를 내려쳐라!</h3>
 
+      <Body className="Body d-flex">
+        <br />
+        <Arm></Arm>
+      </Body>
+    </>
+  );
+};
+const Body = styled.div`
+  height: 100vh;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-content: center;
+
+  .header {
+    text-align: center;
+    font-size: 30px;
+  }
+`;
+const Arm = styled.div`
+  background: pink;
+  width: 300px;
+  height: 100px;
+`;
 export default Play;
