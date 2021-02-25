a922a08bf42645e7ccb8a8d48a14e593d880abd8
acttoz <acttoze@gmail.com>
2021-02-25 20:18:26 +0900

    moon  세팅

---
+++ pages/moon.js
@@ -0,0 +1,6 @@
+import Main from "../src/screeens/moon/main";
+const index = () => {
+  return <Main></Main>;
+};
+
+export default index;
---
+++ src/screeens/moon/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="w-100 bg-warning container d-flex flex-column justify-content-around vh-100">
+      <div className="d-flex justify-content-center align-items-center">
+        <h1>왼발 딱지</h1>
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <button className="btn btn-primary btn-lg">시작하기</button>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
