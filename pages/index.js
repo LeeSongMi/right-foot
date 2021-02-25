5798ea11e110f0ecf8376eb1c3d4185c19b124ef
LeeSongMi <lssong9631@gmail.com>
2021-02-25 20:26:10 +0900

    내폴더 생성

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const index = () => {
-  return <div>index</div>;
+  return (
+    <>
+      <h1>시작화면입니다.</h1>
+    </>
+  );
 };
 
 export default index;
