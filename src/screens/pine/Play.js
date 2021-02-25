d79578f0730aef355185860fb24076dc3855f907
LeeSongMi <lssong9631@gmail.com>
2021-02-25 21:55:06 +0900

    팔 붙임

--- src/screens/pine/Play.js
+++ src/screens/pine/Play.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
-
 const Play = () => {
+  const timing = useRef(10);
+
   return (
     <>
       <h3 className="w-100 text-center">딱지를 내려쳐라!</h3>
-
-      <Body className="Body d-flex">
-        <br />
-        <Arm></Arm>
+      <Body className="Body position-relative">
+        <Arm
+          className="arm position-absolute"
+          src={"/arm.png"}
+          timing={timing}
+        />
+        <Man className="man position-absolute" src={"/man.png"} />
       </Body>
     </>
   );
@@ -18,15 +22,24 @@ const Body = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   align-content: center;
+  background: pink;
 
   .header {
     text-align: center;
     font-size: 30px;
   }
 `;
-const Arm = styled.div`
-  background: pink;
-  width: 300px;
-  height: 100px;
+const Arm = styled.img`
+  height: 30vh;
+  left: 40%;
+  top: 10%;
+  transform-origin: 100% 100%;
+  transform: rotate(30deg);
+`;
+
+const Man = styled.img`
+  height: 75vh;
+  left: 50%;
+  top: 10%;
 `;
 export default Play;
